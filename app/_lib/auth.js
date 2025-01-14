e8882accca9d2e768710aa903e079a94e1cfd287
Trp Singh <trpsingh12@gmail.com>
2025-01-14 23:31:31 +0530

    middleware in next

---
+++ app/_lib/auth.js
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+
+const authConfig = {
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
+        }),
+    ],
+    callbacks: {
+        authorized({ auth, response }) {
+            return !!auth?.user;
+        }
+    }
+}
+
+export const { auth, handlers: { GET, POST } } = NextAuth(authConfig)
---
+++ middleware.js
@@ -0,0 +1,11 @@
+// import { NextResponse } from "next/server";
+import { auth } from "./app/_lib/auth";
+
+
+// export function middleware(request) {
+//     return NextResponse.redirect(new URL('/about', request.url));
+// }
+export const middleware = auth
+export const config = {
+    matcher: ['/account']
+}
